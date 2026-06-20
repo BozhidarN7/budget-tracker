30bda368af99c4075f97ffa8cdbd1164c535787e
Bozhidar Nemski <bojonemski@gmail.com>
2026-06-20 13:32:15 +0300

    feat(view-transitions): animate between recent transactions and transactions apge

--- src/components/Transactions/TransactionListWithFilters/TransactionListWithFilters.tsx
+++ src/components/Transactions/TransactionListWithFilters/TransactionListWithFilters.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { ViewTransition } from 'react';
 import TransactionFilters from '../TransactionFilters';
 import TransactionList from '../TransactionList';
 import { useBudgetData, useTransactionFilters } from '@/hooks/';
+import { TRANSACTIONS_LIST_DASHBOARD_TRANSACTIONS } from '@/constants';
 
 export default function TransactionListWithFilters() {
   const {
@@ -29,16 +31,18 @@ export default function TransactionListWithFilters() {
         onFilterChange={updateFilter}
         onClearFilters={clearFilters}
       />
-      <TransactionList
-        transactions={filteredTransactions}
-        isFilteredEmpty={isFilteredEmpty}
-        isLoading={isLoading || transactionPagination.isLoadingInitial}
-        isLoadingMore={transactionPagination.isLoadingMore}
-        hasMore={transactionPagination.hasMore}
-        loadMoreError={transactionPagination.error}
-        onLoadMore={loadMoreTransactions}
-        showLoadedFilterNote={hasFiltersApplied}
-      />
+      <ViewTransition name={TRANSACTIONS_LIST_DASHBOARD_TRANSACTIONS}>
+        <TransactionList
+          transactions={filteredTransactions}
+          isFilteredEmpty={isFilteredEmpty}
+          isLoading={isLoading || transactionPagination.isLoadingInitial}
+          isLoadingMore={transactionPagination.isLoadingMore}
+          hasMore={transactionPagination.hasMore}
+          loadMoreError={transactionPagination.error}
+          onLoadMore={loadMoreTransactions}
+          showLoadedFilterNote={hasFiltersApplied}
+        />
+      </ViewTransition>
     </div>
   );
 }
--- src/constants/view-transitions-names.ts
+++ src/constants/view-transitions-names.ts
@@ -1,3 +1,6 @@
 export const EXPENSES_BY_CATEGORY_TITLE = 'expenses-by-category-title';
 export const EXPENSES_BY_CATEGORY_CHART = 'expenses-by-category-chart';
 export const EXPENSES_BY_CATEGORY_LEGENG = 'expenses-by-category-legend';
+
+export const TRANSACTIONS_LIST_DASHBOARD_TRANSACTIONS =
+  'transactions-list-dashboard-transactions';
